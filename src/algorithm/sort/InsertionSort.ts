72c06bb5a51885c493151bc0ee140b3d69ed0ec7
xuyongli <xuyongli@live.cn>
2019-05-12 01:06:08 +0800

    feat: 插入排序

---
+++ src/algorithm/sort/InsertionSort.ts
@@ -0,0 +1,40 @@
+// 插入排序
+function InsertionSort(arr: number[]): number[] {
+  let i = 1
+  let len = arr.length
+  while(i < len) {
+    let j = i
+    while(j > 0 && arr[j - 1] > arr[j]) {
+      [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+      j--
+    }
+    i++
+  }
+  return arr
+}
+
+
+function binaryInsertionSort(arr: number[]): number[] {
+  const arrLen = arr.length
+  let left, right, i, j
+  for (i = 1; i < arrLen; i++) {
+    left = 0
+    right = i - 1
+    const cur = arr[i]
+    while (left <= right) {
+      const middle = Math.floor((left + right) / 2)
+      if (cur < arr[middle]) {
+        right = middle - 1
+      } else {
+        left = middle + 1
+      }
+    }
+    for (j = i - 1; j >= left; j--) {
+      arr[j + 1] = arr[j]
+    }
+    arr[left] = cur
+  }
+  return arr
+}
+
+console.log(binaryInsertionSort([3, 5, 1, 4, 2]))
