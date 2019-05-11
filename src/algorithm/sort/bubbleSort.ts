07db6bb18c61f4ac5886f2c16ba21fec01e216c4
xuyongli <xuyongli@live.cn>
2019-05-12 00:43:20 +0800

    feat: 冒泡排序优化

--- src/algorithm/sort/bubbleSort.ts
+++ src/algorithm/sort/bubbleSort.ts
@@ -11,4 +11,44 @@ function bubbleSort(arr: number[]): number[] {
   return arr
 }
 
-console.log(bubbleSort([3, 5, 1, 4, 2]))
+// 优化1
+function bubbleSort2(arr: number[]): number[] {
+  let i = arr.length - 1 //初始时,最后位置保持不变
+  let j, pos
+  while (i > 0) {
+    pos= 0 //每趟开始时,无记录交换
+    for (j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        pos= j //记录交换的位置
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+    i = pos; //为下一趟排序作准备
+  }
+  return arr
+}
+
+// 优化2
+function bubbleSort3(arr: number[]): number[] {
+  let low = 0
+  let high = arr.length - 1 //设置变量的初始值
+  let j
+  while (low < high) {
+    for (j = low; j < high; j++) { //正向冒泡,找到最大者
+      if (arr[j] > arr[j + 1]) {
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+    high-- //修改high值, 前移一位
+    for (j = high; j > low; j--) { //反向冒泡,找到最小者
+      if (arr[j] < arr[j - 1]) {
+        ;[arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+      }
+    }
+    low++ //修改low值,后移一位
+  }
+  return arr
+}
+
+const arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48]
+console.table(bubbleSort3(arr))
